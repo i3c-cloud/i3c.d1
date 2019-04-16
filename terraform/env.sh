659e14327270b97ccd64489aa8cb10938a6b26aa
i3c <i3c@o2.pl>
2019-04-16 21:11:37 +0000

    ansible host access

--- terraform/env.sh
+++ terraform/env.sh
@@ -2,3 +2,4 @@
 export TF_VAR_aws_access_key=$(/r secret awsAccessKey)
 export TF_VAR_aws_secret_key=$(/r secret awsSecretKey)
 export TF_VAR_aws_region=$(/r secret awsRegion)
+export TF_VAR_ssh_key_name=test1.pem
--- terraform/example.tf
+++ terraform/example.tf
@@ -1,9 +1,16 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+variable "ssh_key_name" {}
+
 variable "aws_region" {
   default = "us-east-1"
 }
 
+/*resource "aws_key_pair" "test1" {
+  key_name   = "test1"
+  public_key = "${var.aws_keypair_test1}"
+}*/
 
 provider "aws" {
   access_key = "${var.aws_access_key}"
@@ -11,16 +18,46 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-resource "aws_instance" "example" {
+/*resource "aws_instance" "Ansible-Control-Server" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+  key_name = "test1"
 
-provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+/*provisioner "local-exec" {
+    //command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
   }
+}*/
+
+
+resource "aws_instance" "Ansible-Host-1" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+  key_name = "test1"
+
+/*provisioner "local-exec" {
+    command = " &&  && "
+  }
+*/
+  provisioner "remote-exec" {
+    inline = [
+      //"sudo apt-get -y update",
+      "sudo adduser --disabled-password --gecos \"\" ansible",
+      "sudo adduser ansible sudo",
+      "sudo cp -R /home/ubuntu/.ssh /home/ansible/",
+      "sudo chown -R ansible:ansible /home/ansible/.ssh"
+    ],
+      connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    //password = "${var.root_password}"
+     private_key   = "${file("/run/secrets/sshKeys/${var.ssh_key_name}")}"
+  }
+  }
+
 }
 
-resource "aws_eip" "ip" {
+
+/*resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
-}
+}*/
 
