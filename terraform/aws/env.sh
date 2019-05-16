43f9205b349356e7ee6c4548d8157b45d5866277
Virtimus <virtimus@gmail.com>
2019-05-16 19:41:28 +0200

    awsfix

--- terraform/aws/env.sh
+++ terraform/aws/env.sh
@@ -1,5 +1,5 @@
 
-export TF_VAR_aws_access_key=$(/r secret awsAccessKey)
-export TF_VAR_aws_secret_key=$(/r secret awsSecretKey)
-export TF_VAR_aws_region=$(/r secret awsRegion)
+export TF_VAR_aws_access_key=$(/r secret aws/awsAccessKey)
+export TF_VAR_aws_secret_key=$(/r secret aws/awsSecretKey)
+export TF_VAR_aws_region=$(/r secret aws/awsRegion)
 export TF_VAR_ssh_key_name=test1.pem
